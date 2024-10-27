98acfa23019225ff0c0f511f81fc57689117f5da
CantarinoG <guilhermecantarino1709@gmail.com>
2024-10-27 18:51:46 -0300

    feat: Definir constantes de cores (Sprint 2)

---
+++ src/main/java/com/cantarino/souza/components/AppColors.java
@@ -0,0 +1,16 @@
+package com.cantarino.souza.components;
+
+import java.awt.Color;
+
+public class AppColors {
+
+    public static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+    public static final Color BUTTON_PINK = new Color(255, 153, 153, 255);
+    public static final Color FIELD_PINK = new Color(255, 228, 228, 255);
+    public static final Color TITLE_BLUE = new Color(38, 47, 113, 255);
+    public static final Color DARKER = new Color(0, 0, 0, 10);
+
+    private AppColors() {
+    }
+
+}
