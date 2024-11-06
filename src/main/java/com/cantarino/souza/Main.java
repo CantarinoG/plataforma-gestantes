66b7279959d3a843de5bc07d0ff55180d9d47351
so-tha <thais3225@gmail.com>
2024-11-06 10:02:03 -0300

    feat: Desenvolver View de Cadastro de Gestantes

--- src/main/java/com/cantarino/souza/Main.java
+++ src/main/java/com/cantarino/souza/Main.java
@@ -4,11 +4,12 @@ import com.cantarino.souza.view.*;
 
 public class Main {
     public static void main(String[] args) {
-        // FrLogin frame = new FrLogin();
+        //FrLogin frame = new FrLogin();
         // FrOrientacoes frame = new FrOrientacoes();
         // FrComunidade frame = new FrComunidade();
         // FrAgendaConsultas frame = new FrAgendaConsultas();
-        FrPagamentos frame = new FrPagamentos();
+        //FrPagamentos frame = new FrPagamentos();
+        FrCadastroGestantes frame = new FrCadastroGestantes();
         frame.setVisible(true);
     }
 }
