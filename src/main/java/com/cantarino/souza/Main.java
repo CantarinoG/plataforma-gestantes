b57c9d314e8568c18f9357ca5b13e2eeeb1a2c82
CantarinoG <guilhermecantarino1709@gmail.com>
2024-11-10 17:25:14 -0300

    refactor: Adicionar package separado para as telas (Sprint 3)

--- src/main/java/com/cantarino/souza/Main.java
+++ src/main/java/com/cantarino/souza/Main.java
@@ -1,6 +1,6 @@
 package com.cantarino.souza;
 
-import com.cantarino.souza.view.*;
+import com.cantarino.souza.view.screens.*;
 
 public class Main {
         public static void main(String[] args) {
