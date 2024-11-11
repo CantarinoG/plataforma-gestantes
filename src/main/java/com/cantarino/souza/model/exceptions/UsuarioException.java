885dd26eb8541965acdd350f71dbfd033d364a11
CantarinoG <guilhermecantarino1709@gmail.com>
2024-11-11 17:29:50 -0300

    feat: Adicionar Validação de Usuário (Sprint 3)

---
+++ src/main/java/com/cantarino/souza/model/exceptions/UsuarioException.java
@@ -0,0 +1,9 @@
+package com.cantarino.souza.model.exceptions;
+
+public class UsuarioException extends RuntimeException {
+
+    public UsuarioException(String msg) {
+        super(msg);
+    }
+
+}
