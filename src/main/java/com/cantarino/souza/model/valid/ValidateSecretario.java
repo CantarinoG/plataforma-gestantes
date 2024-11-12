6d6d5be726e02c9080f7316bc4d5ecc811857106
CantarinoG <guilhermecantarino1709@gmail.com>
2024-11-12 08:51:32 -0300

    feat: Adicionar Validação de Secretário (Sprint 3)

---
+++ src/main/java/com/cantarino/souza/model/valid/ValidateSecretario.java
@@ -0,0 +1,41 @@
+package com.cantarino.souza.model.valid;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+import com.cantarino.souza.model.entities.Secretario;
+import com.cantarino.souza.model.entities.Usuario;
+import com.cantarino.souza.model.exceptions.SecretarioException;
+
+public class ValidateSecretario extends ValidateUsuario {
+
+    public Secretario validaCamposEntrada(String cpf, String nome, String email, String senha, String dataNascimento,
+            String telefone, String endereco, String deletadoEm, String dataContratacao) {
+
+        Secretario secretario = new Secretario();
+        Usuario base = super.validaCamposEntrada(cpf, nome, email, senha, dataNascimento, telefone, endereco,
+                deletadoEm);
+        secretario.setCpf(base.getCpf());
+        secretario.setNome(base.getNome());
+        secretario.setEmail(base.getEmail());
+        secretario.setSenha(base.getSenha());
+        secretario.setDataNascimento(base.getDataNascimento());
+        secretario.setTelefone(base.getTelefone());
+        secretario.setEndereco(base.getEndereco());
+        secretario.setDeletadoEm(base.getDeletadoEm());
+
+        if (dataContratacao == null || dataContratacao.isEmpty())
+            throw new SecretarioException("ERRO: Campo data de contratação não pode ser vazio.");
+        LocalDate date;
+        try {
+            date = LocalDate.parse(dataContratacao);
+        } catch (DateTimeParseException e) {
+            throw new SecretarioException("ERRO: Formato de data inválido.");
+        }
+        secretario.setDataContratacao(date);
+
+        return secretario;
+
+    }
+
+}
