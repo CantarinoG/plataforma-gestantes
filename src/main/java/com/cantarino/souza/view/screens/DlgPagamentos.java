b404259f1fc92b770ac1f78e050db12b9c4c0b72
CantarinoG <guilhermecantarino1709@gmail.com>
2024-11-13 17:49:54 -0300

    feat: Adicionar renderização condicional na view de Pagamentos de acordo com usuário (Sprint 3)

--- src/main/java/com/cantarino/souza/view/screens/DlgPagamentos.java
+++ src/main/java/com/cantarino/souza/view/screens/DlgPagamentos.java
@@ -1,19 +1,25 @@
 package com.cantarino.souza.view.screens;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import com.cantarino.souza.view.components.*;
 
 public class DlgPagamentos extends JDialog {
 
     JPanel panBackground;
-    JPanel panHeader;
-    JLabel lblTitle;
-    JPanel panContent;
     JLabel lblAction;
     JPanel panTable;
     JPanel panOptions;
     JButton btnEmitirRecibo;
+    JButton btnRegistrarPagamento;
+    JButton btnEditarPagamento;
+    JButton btnDeletarPagamento;
+    JTable grdPagamentos;
+    JScrollPane scrollPane;
 
     public DlgPagamentos(JFrame parent, boolean modal) {
         super(parent, modal);
@@ -27,68 +33,50 @@ public class DlgPagamentos extends JDialog {
         setLocationRelativeTo(null);
 
         panBackground = new BackgroundPanel("/images/background.png");
-        panBackground.setLayout(new BorderLayout());
+        panBackground.setLayout(new GridBagLayout());
+        panBackground.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         setContentPane(panBackground);
-
-        panHeader = new JPanel();
-        panHeader.setLayout(new BorderLayout());
-        panHeader.setBackground(AppColors.DARKER);
-        panHeader.setPreferredSize(new Dimension(getWidth(), 74));
-        panHeader.setBorder(BorderFactory.createEmptyBorder(15, 64, 15, 64));
-        panBackground.add(panHeader, BorderLayout.NORTH);
-
-        lblTitle = new JLabel("BemGestar");
-        lblTitle.setFont(new Font("Arial", Font.BOLD, 32));
-        panHeader.add(lblTitle, BorderLayout.WEST);
-
-        panContent = new JPanel();
-        panContent.setLayout(new GridBagLayout());
-        panContent.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-        panContent.setBackground(AppColors.TRANSPARENT);
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 1.0;
 
-        lblAction = new JLabel("Histórico de Pagamentos");
+        lblAction = new JLabel("Gestão de Pagamentos");
         lblAction.setFont(new Font("Arial", Font.BOLD, 64));
         lblAction.setForeground(AppColors.TITLE_BLUE);
         lblAction.setHorizontalAlignment(SwingConstants.CENTER);
-        panContent.add(lblAction, gbc);
+        panBackground.add(lblAction, gbc);
 
-        /*
-         * panTable = new JPanel();
-         * panTable.setBackground(AppColors.TITLE_BLUE);
-         * gbc.gridy = 1;
-         * gbc.weighty = 1.0;
-         * gbc.fill = GridBagConstraints.BOTH;
-         * panContent.add(panTable, gbc);
-         */
+        grdPagamentos = new JTable();
+        grdPagamentos.setModel(new DefaultTableModel(
+                new Object[][] {
+                        {},
+                        {},
+                        {},
+                        {}
+                },
+                new String[] {
 
-        {// Tabela mockada
-            String[] columnNames = { "Data", "Hora", "Médico", "Especialidade", "Status" };
-            Object[][] data = {
-                    { "2024-03-20", "14:30", "Dr. Silva", "Obstetrícia", "Agendada" },
-                    { "2024-03-25", "10:00", "Dra. Santos", "Ginecologia", "Agendada" }
-            };
-
-            JTable tblConsultas = new JTable(data, columnNames);
-            tblConsultas.setFillsViewportHeight(true);
-            tblConsultas.setRowHeight(30);
-            tblConsultas.getTableHeader().setFont(new Font("Arial", Font.BOLD, 12));
-
-            JScrollPane scrollPane = new JScrollPane(tblConsultas);
-            scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-            panContent.add(scrollPane, gbc);
-        }
+                }));
+        grdPagamentos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                grdPagamentosMouseClicked(evt);
+            }
+        });
+        grdPagamentos.setFillsViewportHeight(true);
+        grdPagamentos.setBackground(AppColors.FIELD_PINK);
+        scrollPane = new JScrollPane();
+        scrollPane.setViewportView(grdPagamentos);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panBackground.add(scrollPane, gbc);
 
         panOptions = new JPanel(new FlowLayout(FlowLayout.CENTER));
         panOptions.setBackground(AppColors.TRANSPARENT);
         gbc.gridy = 2;
         gbc.weighty = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
-        panContent.add(panOptions, gbc);
+        panBackground.add(panOptions, gbc);
 
         btnEmitirRecibo = new RoundedButton("Emitir Recibo", 50);
         btnEmitirRecibo.setPreferredSize(new Dimension(200, 55));
@@ -100,13 +88,92 @@ public class DlgPagamentos extends JDialog {
                 btnEmitirReciboActionPerformed(evt);
             }
         });
-        panOptions.add(btnEmitirRecibo);
 
-        panBackground.add(panContent, BorderLayout.CENTER);
+        btnRegistrarPagamento = new RoundedButton("Registrar Pagamento", 50);
+        btnRegistrarPagamento.setPreferredSize(new Dimension(200, 55));
+        btnRegistrarPagamento.setMinimumSize(new Dimension(200, 55));
+        btnRegistrarPagamento.setMaximumSize(new Dimension(200, 55));
+        btnRegistrarPagamento.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnRegistrarPagamento.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarPagamentoActionPerformed(evt);
+            }
+        });
+
+        btnEditarPagamento = new RoundedButton("Editar Pagamento", 50);
+        btnEditarPagamento.setPreferredSize(new Dimension(200, 55));
+        btnEditarPagamento.setMinimumSize(new Dimension(200, 55));
+        btnEditarPagamento.setMaximumSize(new Dimension(200, 55));
+        btnEditarPagamento.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnEditarPagamento.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditarPagamentoActionPerformed(evt);
+            }
+        });
+
+        btnDeletarPagamento = new RoundedButton("Deletar Pagamento", 50);
+        btnDeletarPagamento.setPreferredSize(new Dimension(200, 55));
+        btnDeletarPagamento.setMinimumSize(new Dimension(200, 55));
+        btnDeletarPagamento.setMaximumSize(new Dimension(200, 55));
+        btnDeletarPagamento.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        btnDeletarPagamento.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeletarPagamentoActionPerformed(evt);
+            }
+        });
+
+        initUserCoponents();
+
+    }
+
+    private void initUserCoponents() {
+        String userType = "GESTANTE";
+
+        if (userType == "GESTANTE") {
+            initGestanteComponents();
+        } else if (userType == "SECRETARIO") {
+            initSecretarioComponents();
+        } else if (userType == "ADM") {
+            initAdmComponents();
+        }
+    }
+
+    private void initGestanteComponents() {
+        panOptions.add(btnEmitirRecibo);
+    }
+
+    private void initSecretarioComponents() {
+        panOptions.add(btnEmitirRecibo);
+        panOptions.add(btnRegistrarPagamento);
+    }
+
+    private void initAdmComponents() {
+        panOptions.add(btnEmitirRecibo);
+        panOptions.add(btnRegistrarPagamento);
+        panOptions.add(btnEditarPagamento);
+        panOptions.add(btnDeletarPagamento);
     }
 
     private void btnEmitirReciboActionPerformed(java.awt.event.ActionEvent evt) {
         System.out.println("Clicou no botão de emitir recibo!");
+    }
+
+    private void grdPagamentosMouseClicked(java.awt.event.MouseEvent evt) {
+        if (evt.getClickCount() == 2) {
+
+        }
+    }
+
+    private void btnRegistrarPagamentoActionPerformed(java.awt.event.ActionEvent evt) {
+        System.out.println("Clicou em registrar pagamento");
+    }
+
+    private void btnEditarPagamentoActionPerformed(java.awt.event.ActionEvent evt) {
+        System.out.println("Clicou em editar pagamento");
+    }
+
+    private void btnDeletarPagamentoActionPerformed(java.awt.event.ActionEvent evt) {
+        System.out.println("Clicou em deletar pagamento");
     }
 
 }
