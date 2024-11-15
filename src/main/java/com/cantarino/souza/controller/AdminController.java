3f91c665bbb9bf32f634ae02361ec615a9941575
CantarinoG <guilhermecantarino1709@gmail.com>
2024-11-15 11:08:32 -0300

    feat: Adicionar lógica para cadastro de Admins (Sprint 3)

--- src/main/java/com/cantarino/souza/controller/AdminController.java
+++ src/main/java/com/cantarino/souza/controller/AdminController.java
@@ -3,17 +3,31 @@ package com.cantarino.souza.controller;
 import javax.swing.JTable;
 
 import com.cantarino.souza.model.dao.AdminDao;
+import com.cantarino.souza.model.entities.Admin;
+import com.cantarino.souza.model.valid.ValidateAdmin;
 
 public class AdminController {
 
     private AdminDao repositorio;
+    private ValidateAdmin validator;
 
     public AdminController() {
         repositorio = new AdminDao();
+        validator = new ValidateAdmin();
     }
 
     public void atualizarTabela(JTable grd) {
         Util.jTableShow(grd, new TMAdmin(repositorio.findAll()), null);
+    }
+
+    public void cadastrar(String cpf, String nome, String email, String senha, String dataNascimento,
+            String telefone, String endereco, String deletadoEm) {
+
+        Admin novoAdm = validator.validaCamposEntrada(cpf, nome, email, senha, dataNascimento, telefone, endereco,
+                deletadoEm);
+
+        repositorio.save(novoAdm);
+
     }
 
 }
