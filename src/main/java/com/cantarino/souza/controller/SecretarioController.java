80e4c0dc797674f76978bc96b581ba023cbf76f0
CantarinoG <guilhermecantarino1709@gmail.com>
2024-11-15 11:03:14 -0300

    feat: Adicionar lógica para cadastro de Secretária (Sprint 3)

--- src/main/java/com/cantarino/souza/controller/SecretarioController.java
+++ src/main/java/com/cantarino/souza/controller/SecretarioController.java
@@ -3,17 +3,31 @@ package com.cantarino.souza.controller;
 import javax.swing.JTable;
 
 import com.cantarino.souza.model.dao.SecretarioDao;
+import com.cantarino.souza.model.entities.Secretario;
+import com.cantarino.souza.model.valid.ValidateSecretario;
 
 public class SecretarioController {
 
     private SecretarioDao repositorio;
+    private ValidateSecretario validator;
 
     public SecretarioController() {
         repositorio = new SecretarioDao();
+        validator = new ValidateSecretario();
     }
 
     public void atualizarTabela(JTable grd) {
         Util.jTableShow(grd, new TMSecretario(repositorio.findAll()), null);
+    }
+
+    public void cadastrar(String cpf, String nome, String email, String senha, String dataNascimento,
+            String telefone, String endereco, String deletadoEm, String dataContratacao) {
+
+        Secretario novoSecretario = validator.validaCamposEntrada(cpf, nome, email, senha, dataNascimento, telefone,
+                endereco, deletadoEm, dataContratacao);
+
+        repositorio.save(novoSecretario);
+
     }
 
 }
