9adb22eb89aa3dde46436e25b97ff88f9788ead4
so-tha <thais3225@gmail.com>
2024-11-23 07:23:00 -0300

    feat: Implementar CRUD Comentário (Sprint 3)

--- src/main/java/com/cantarino/souza/controller/PublicacaoController.java
+++ src/main/java/com/cantarino/souza/controller/PublicacaoController.java
@@ -3,6 +3,8 @@ package com.cantarino.souza.controller;
 
 import java.time.LocalDateTime;
 
+import javax.swing.JTable;
+
 import com.cantarino.souza.model.dao.PublicacaoDao;
 import com.cantarino.souza.model.entities.Publicacao;
 import com.cantarino.souza.model.entities.Usuario;
@@ -11,6 +13,10 @@ import com.cantarino.souza.model.valid.ValidatePublicacao;
 public class PublicacaoController {
     private PublicacaoDao repositorio;
     private ValidatePublicacao validator;
+
+    public void atualizarTabela(JTable grd){
+        Util.jTableShow(grd,new TMPublicacao(repositorio.findAll()),null);
+    }
 
     public PublicacaoController(){
         this.repositorio = new PublicacaoDao();
@@ -24,7 +30,7 @@ public class PublicacaoController {
         novaPublicacao.setMedico(isMedico);
         repositorio.save(novaPublicacao);
     }
-    public void atualizar(String titulo, String corpo, LocalDateTime data, boolean isMedico, Usuario autor, LocalDateTime deletadoEm){
+    public void atualizar(int id,String titulo, String corpo, LocalDateTime data, boolean isMedico, Usuario autor, LocalDateTime deletadoEm){
         Publicacao novaPublicacao = validator.validaCamposEntrada(titulo, corpo, isMedico);
         novaPublicacao.setAutor(autor);
         novaPublicacao.setCorpo(corpo);
