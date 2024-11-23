5802e69d50317e4e8c2b2bfa1bbe0215bf0198bc
so-tha <thais3225@gmail.com>
2024-11-23 08:37:14 -0300

    feat: Implementar CRUD Relatório (Sprint 3)

---
+++ src/main/java/com/cantarino/souza/controller/RelatorioController.java
@@ -0,0 +1,42 @@
+package com.cantarino.souza.controller;
+
+import java.time.LocalDateTime;
+
+import javax.swing.JTable;
+
+import com.cantarino.souza.model.dao.RelatorioDao;
+import com.cantarino.souza.model.entities.Procedimento;
+import com.cantarino.souza.model.entities.Relatorio;
+import com.cantarino.souza.model.valid.ValidateRelatorio;
+
+public class RelatorioController {
+    private RelatorioDao repositorio;
+    private ValidateRelatorio validator;
+
+    public RelatorioController(){
+        this.repositorio = new RelatorioDao();
+        this.validator = new ValidateRelatorio();
+    }
+    public void atualizarTabela(JTable grd){
+        Util.jTableShow(grd,new TMRelatorio(repositorio.findAll()),null);
+    }
+    public void cadastrar(LocalDateTime dataEmissao, Procedimento procedimento, String resultado, String observacoes, LocalDateTime deletadoEm){
+        Relatorio novoRelatorio = validator.validaCamposEntrada(resultado, observacoes);
+        novoRelatorio.setResultado(resultado);
+        novoRelatorio.setObeservacoes(observacoes);
+        novoRelatorio.setProcedimento(procedimento);
+        repositorio.save(novoRelatorio);
+    }
+    public void atualizar(int id,LocalDateTime dataEmissao, Procedimento procedimento, String resultado, String observacoes, LocalDateTime deletadoEm){
+        Relatorio novoRelatorio = validator.validaCamposEntrada(resultado, observacoes);
+        novoRelatorio.setId(id);
+        novoRelatorio.setResultado(resultado);
+        novoRelatorio.setObeservacoes(observacoes);
+        novoRelatorio.setProcedimento(procedimento);
+        repositorio.update(novoRelatorio);
+    }
+    public void excluir(int id){
+        Relatorio relatorio = repositorio.find(id);
+        repositorio.delete(relatorio);
+    }
+}
